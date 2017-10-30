582fb5236d4ffa5d0b66e5080e6ca97cab35c64a
Bimalkant Lauhny <lauhny.bimalk@gmail.com>
2017-10-30 14:01:59 +0530

    Lamport Screenshot

--- lamportClock.cpp
+++ lamportClock.cpp
@@ -11,14 +11,14 @@ int max1(int a, int b) {
 
 int main() {
     int i,j,k,p1[20],p2[20],e1,e2,dep[20][20];
-    printf("enter the events : ");
-    scanf("%d %d",&e1,&e2);
+    printf("Enter the events: ");
+    scanf("%d %d", &e1, &e2);
     for(i=0;i<e1;i++)
         p1[i]=i+1;
     for(i=0;i<e2;i++)
         p2[i]=i+1;
-    printf("enter the dependency matrix:\n");
-    printf("\t enter 1 if e1->e2 \n\t enter -1, if e2->e1 \n\t else enter 0 \n\n");
+    printf("Enter the dependency matrix:\n");
+    printf("\t Enter 1 if e1->e2 \n\t Enter -1, if e2->e1 \n\t else Enter 0 \n\n");
     for(i=0;i<e2;i++)
         printf("\te2%d",i+1);
     for(i=0;i<e1;i++) {
@@ -45,12 +45,14 @@ int main() {
         }
     }
     //to print the outcome of Lamport Logical Clock
+    printf("\n");
     printf("P1 : ");     
     for(i=0;i<e1;i++)
-        printf("%d",p1[i]);
-    printf("\n P2 : ");
+        printf("%d ",p1[i]);
+    printf("\n");
+    printf("P2 : ");
     for(j=0;j<e2;j++)
-        printf("%d",p2[j]);
-
+        printf("%d ",p2[j]);
+    printf("\n");
     return 0 ;
 }
